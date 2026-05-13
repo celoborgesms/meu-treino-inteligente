a57f4db05ad70c9a5d1e7a2f954b68393ebe9b9f
celoborgesms <celoborgesms@gmail.com>
2026-05-13 10:38:33 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "metatreino-cache-v0.6.2-restaurar-sugestao";
+const CACHE_NAME = "metatreino-cache-v0.7.0-ilustracoes-locais";
 
 const APP_SHELL = [
   "./",
-  "./index.html?v=0.6.2",
-  "./manifest.json?v=0.6.2",
-  "./icon-512.png.png?v=0.6.2"
+  "./index.html?v=0.7.0",
+  "./manifest.json?v=0.7.0",
+  "./icon-512.png.png?v=0.7.0"
 ];
 
 // INSTALACAO
@@ -54,13 +54,13 @@ self.addEventListener("fetch", function(event) {
         .then(function(response) {
           const copy = response.clone();
           caches.open(CACHE_NAME).then(function(cache) {
-            cache.put("./index.html?v=0.6.2", copy);
+            cache.put("./index.html?v=0.7.0", copy);
             cache.put("./index.html", response.clone());
           });
           return response;
         })
         .catch(function() {
-          return caches.match("./index.html?v=0.6.2").then(function(cached) {
+          return caches.match("./index.html?v=0.7.0").then(function(cached) {
             return cached || caches.match("./index.html");
           });
         })
